030d3f47932c74bdcd437ca8dbdbc5a868668aba
Cedric <cedric.frachisse@gmail.com>
2024-03-13 17:27:42 +0100

    first

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ingredient;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $ingredient = new Ingredient();
+        $ingredient->setName('Ingredient #1')
+        ->setPrice(3.0); 
+        
+        $manager->persist($ingredient);
+
+        $manager->flush();
+    }
+}
