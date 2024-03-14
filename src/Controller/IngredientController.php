e64e160d095870f08ab8eddcb3ade1ca2be175ed
Cedric <cedric.frachisse@gmail.com>
2024-03-14 08:18:12 +0100

    14/03 am

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IngredientController extends AbstractController
+{
+    /**
+     * This function display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'app_ingredient', methods: ['GET'])]
+    public function index(IngredientRepository $repository, PaginatorInterface  $paginator, Request $request): Response
+    {
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('pages/ingredient/index.html.twig', [
+            'ingredients' => $ingredients
+        ]);
+    }
+
+    // equivaut à 
+
+    // public function index(IngredientRepository $repository): Response
+    // {
+    //     $ingredients = $repository->findAll();
+
+
+    //     return $this->render('pages/ingredient/index.html.twig', [
+    //         'ingredients' => $ingredients
+    //     ]);
+    // }
+    #[Route('/ingredient/nouveau', 'ingredient.new', methods: ['GET', 'POST'])]
+    public function new(): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,19 +5,31 @@ namespace App\DataFixtures;
 use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Generator;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+
+    private Generator $faker;
+
+    public function __construct()
+    {
+        // prof fait juste Factory::create('fr_FR')
+        $this->faker = \Faker\Factory::create('fr_FR');
+    }
     public function load(ObjectManager $manager): void
     {
+        for ($i=1; $i < 50; $i++) {
+
         // $product = new Product();
         // $manager->persist($product);
-        $ingredient = new Ingredient();
-        $ingredient->setName('Ingredient #1')
-        ->setPrice(3.0); 
+            $ingredient = new Ingredient();
+            $ingredient->setName($this->faker->word())
+            ->setPrice(mt_rand(1,100)); 
         
-        $manager->persist($ingredient);
-
+            $manager->persist($ingredient);
+        }
         $manager->flush();
     }
 }
